70867c3257e054f8dbe0b0942b8f666c9db968db
Adrien <adrienvilleneuve21@gmail.com>
2023-10-25 17:11:51 +0200

    MiniProjet Lightsoff

--- TP1_VILLENEUVE/LightOff_VILLENEUVE_version_console/src/lightoff_villeneuve_version_console/GrilleDeJeu.java
+++ TP1_VILLENEUVE/LightOff_VILLENEUVE_version_console/src/lightoff_villeneuve_version_console/GrilleDeJeu.java
@@ -1,18 +1,50 @@
 
 package lightoff_villeneuve_version_console;
-
+ import java.util.Random;
 public class GrilleDeJeu {
-    private CelluleLumineuse[][] Grille;
+    private CelluleLumineuse[][] matriceCellules;
     private int nbLignes;
     private int nbColonnes;
-    public GrilleDeJeu(int nbLignes, int nbColonnes) {
-    CelluleLumineuse[][] Grille = new CelluleLumineuse[nbLignes][nbColonnes];
+    private boolean etat;
 
-        // Remplir la matriceCellules avec de nouvelles instances de CelluleLumineuse
-        for (int i = 0; i < nbLignes; i++) {
-            for (int j = 0; j < nbColonnes; j++) {
-                Grille[i][j] = new CelluleLumineuse(true) {};
+    public GrilleDeJeu(int p_nbLignes, int p_nbColonnes) {
+        nbLignes = p_nbLignes;
+        nbColonnes = p_nbColonnes;
+
+        matriceCellules = new CelluleLumineuse[nbLignes][nbColonnes];
+
+        for (int x = 0; x < nbLignes; x++) {
+            for (int y = 0; y < nbColonnes; y++) {
+                matriceCellules[x][y] = new CelluleLumineuse(false) {};
             }
         }
+       
+    public eteindreToutesLesCellules() {
+        for (int x = 0; x < nbLignes; x++) {
+            for (int y = 0; y < nbColonnes; y++) {
+                matriceCellules[x][y].eteindreCellule();
+            }
+        }
+        this.etat = etat;
     }
+        public void activerLigneColonneOuDiagonaleAleatoire() {
+            Random random = new Random();
+            int choix = random.nextInt(3); // Générer un nombre aléatoire entre 0 et 2
+
+            if (choix == 0) {
+            int ligne = random.nextInt(nbLignes);
+            for (int j = 0; j < nbColonnes; j++) {
+                matriceCellules[ligne][j].activerCellule();
+            }
+        } else if (choix == 1) {
+            int colonne = random.nextInt(nbColonnes);
+            for (int i = 0; i < nbLignes; i++) {
+                matriceCellules[i][colonne].activerCellule();
+            }
+        } else {
+            for (int i = 0; i < Math.min(nbLignes, nbColonnes); i++) {
+                matriceCellules[i][i].activerCellule();
+            }
+        }
+        }
 }
--- TP1_VILLENEUVE/LightOff_VILLENEUVE_version_console/src/lightoff_villeneuve_version_console/LightOff_VILLENEUVE_version_console.java
+++ TP1_VILLENEUVE/LightOff_VILLENEUVE_version_console/src/lightoff_villeneuve_version_console/LightOff_VILLENEUVE_version_console.java
@@ -1,11 +1,14 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
 package lightoff_villeneuve_version_console;
 
 public class LightOff_VILLENEUVE_version_console {
 
-   
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        GrilleDeJeu grille = new GrilleDeJeu(3, 3); 
 }
